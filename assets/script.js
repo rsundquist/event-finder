f79c37887a979bfad5788bb336e1bc984e1f997b
Kronk <kylec0217@gmail.com>
2021-09-08 21:29:30 -0400

    working searchbar

--- assets/script.js
+++ assets/script.js
@@ -4,17 +4,23 @@ var radioSports = document.getElementById("searchBox")
 var radioMusic = document.getElementById("searchBox")
 var radioOther = document.getElementById("searchBox")
 
-var request
 
-fetch('https://api.seatgeek.com/2/events?geoip=48425 -u MjMxMzE1Njl8MTYzMDM3MTYzMS44ODg0NzI')
-  .then(response => response.json())
-  .then(data => console.log(data))
+
+
+
+
+  
  
 
-$(seachButton).click(function() {
-    console.log("lol")
-    console.log($.get("https://api.seatgeek.com/2/events?geoip=48423 -u MjMxMzE1Njl8MTYzMDM3MTYzMS44ODg0NzI"))
+seachButton.addEventListener("click", function() {
+    
+    var request = 'https://api.seatgeek.com/2/events?client_id=MjMxMzE1Njl8MTYzMDM3MTYzMS44ODg0NzI&geoip=' + seachBar.value
+    
+    fetch(request)
+  .then(response => response.json())
+  .then(data => console.log(data))
     if(radioSports.checked == true){
+
 
     }
     else if (radioMusic.checked == true){
