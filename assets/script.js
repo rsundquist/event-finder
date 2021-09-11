721c5b59a1be3ebe62a8538e634a1d33c3a72df8
Kronk <kylec0217@gmail.com>
2021-09-11 12:03:54 -0400

    pulling up search reults

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,7 @@ var type1 = document.getElementById("type1")
 var dateTime1 = document.getElementById("dateTime1")
 var location1 = document.getElementById("location1")
 var price1 = document.getElementById("price1")
+var results = document.getElementById("results")
 console.log(radioSports)
 console.log(radioMusic)
 console.log(radioOther)
@@ -26,5 +27,25 @@ seachButton.addEventListener("click", function() {
     var requestSports = 'https://api.seatgeek.com/2/events?client_id=MjMxMzE1Njl8MTYzMDM3MTYzMS44ODg0NzI&geoip=' + seachBar.value + '&range=10mi' + taxonomies
     fetch(requestSports)
         .then(response => response.json())
-        .then(data => console.log(data.events))
+        .then(data => {
+            console.log(data.events)
+            var finalHTML = ""
+            for (var i = 0; i < data.events.length ; i++){
+                var article = `
+                    <article>
+                        <p id = "title1">${data.events[i].title}</p>
+                        <p id = type1></p>
+                        <p id = "dateTime1"></p>
+                        <p id = "location1"></p>
+                        <p id = "price1"></p>
+                        <p></p>
+                    </article>
+                `
+                console.log(article)
+                finalHTML += article
+            }
+            results.innerHTML = finalHTML;
+        })
+        
+    
 });
