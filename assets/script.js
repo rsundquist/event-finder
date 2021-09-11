10a989fe4eeea81c180106a0959a68ae8868776a
Kronk <kylec0217@gmail.com>
2021-09-11 11:12:24 -0400

    saving place

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,9 @@ var radioSports = document.getElementById("Sports")
 var radioMusic = document.getElementById("Music")
 var radioOther = document.getElementById("Other")
 var lol = ["events", "meta"]
-console.log(lol[0])
 
 
+console.log(lol)
 
 
   
@@ -21,15 +21,14 @@ seachButton.addEventListener("click", function() {
     if(radioSports.checked == true){
         var requestSports = 'https://api.seatgeek.com/2/events?client_id=MjMxMzE1Njl8MTYzMDM3MTYzMS44ODg0NzI&geoip=' + seachBar.value + '&range=10mi&taxonomies.name=sports'
         fetch(requestSports)
-        .then(response => response.json()) 
+        .then(response => response.json())
         .then(data => console.log(data))
         var searchResults = document.getElementById("searchContainer")
         var title1 = document.getElementById("title1")
         var type1 = document.getElementById("type1")
         var dateTime1 = document.getElementById("dateTime1")
         var location1 = document.getElementById("location1")
-        var prive1 = document.getElementById("price1")
-        console.log(response[0])
+        var price1 = document.getElementById("price1")
         }
     else if (radioMusic.checked == true){
         var requestMusic = 'https://api.seatgeek.com/2/events?client_id=MjMxMzE1Njl8MTYzMDM3MTYzMS44ODg0NzI&geoip=' + seachBar.value + '&range=10mi&taxonomies.name=concert'
