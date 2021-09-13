ba2b176668590b5a3307c6305326af723715d215
Rebecca Sundquist <rsundquist629@gmail.com>
2021-09-13 19:01:12 -0400

    changed color on buttons

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 ## event-finder
 - As a user I will be able to search tickets for nearby events and change the currency for the ticket prices
-- I will be able to see events near me by typing in my city or my zipcode
+- I will be able to see events near me by typing in my zipcode
 - I will then be able to choose an event
 - I will be able to see the current ticket prices for that event
 - I will be able to change the currency for the ticket prices
+
+- Here is the 
+
+- Here is the link for the application: 
+
+
+!['Screenshot'](assets/Images/EventFinder.png "EventFinder")
 
--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,17 @@ var radioSports = document.getElementById("sports")
 var radioMusic = document.getElementById("music")
 var radioOther = document.getElementById("other")
 var searchResults = document.getElementById("searchContainer")
+var title1 = document.getElementById("title1")
+var type1 = document.getElementById("type1")
+var dateTime1 = document.getElementById("dateTime1")
+var location1 = document.getElementById("location1")
+var price1 = document.getElementById("price1")
 var results = document.getElementById("results")
 var currencyExchange = "https://v6.exchangerate-api.com/v6/ab0f110ed559d90d33353768/latest/USD"
 console.log(radioSports)
 console.log(radioMusic)
 console.log(radioOther)
 seachButton.addEventListener("click", function() {
-    var moneyPicker = document.getElementById("moneyPicker").value
-    console.log(moneyPicker)
     var taxonomies = ""
     if (radioSports.checked == true) {
         taxonomies = '&taxonomies.name=sports'
@@ -23,66 +26,29 @@ seachButton.addEventListener("click", function() {
     console.log(radioMusic.checked)
     console.log(radioSports.checked)
     var requestSports = 'https://api.seatgeek.com/2/events?client_id=MjMxMzE1Njl8MTYzMDM3MTYzMS44ODg0NzI&geoip=' + seachBar.value + '&range=10mi' + taxonomies
-    var converter = 
-    fetch(currencyExchange)
-    .then(response => response.json())
-    .then(data =>  {console.log(data)
-        if (moneyPicker == "CAD") {
-            var ratio = data.conversion_rates.CAD
-            converter = parseFloat(ratio)}
-        else if (moneyPicker == "EUR") {
-            var ratio = data.conversion_rates.EUR
-            converter = parseFloat(ratio)}
-        else if (moneyPicker == "JPY") {
-            var ratio = data.conversion_rates.JPY
-            converter = parseFloat(ratio)}
-        else if (moneyPicker == "AUD") {
-            var ratio = data.conversion_rates.AUD
-            converter = parseFloat(ratio)}
-    })
     fetch(requestSports)
         .then(response => response.json())
         .then(data => {
             console.log(data)
             var finalHTML = ""
+           
             for (var i = 0; i < data.events.length ; i++){
                 var price = data.events[i].stats.average_price
-                var priceNum = parseFloat(price)
-            if (price === null){
-                    priceNum = ""
-                }
-                else {priceNum = priceNum*converter 
-                        console.log(priceNum)
-                        priceString =  priceNum.toFixed(2)
-
-                }
-                if (moneyPicker == "USD"){
-                    priceString = "$" + priceString
-                    
-                }
-                else if (moneyPicker == "CAD"){
-                    priceString = "$" + priceString
-                        
-                }
-                else if (moneyPicker == "EUR"){
-                    priceString = "€" + priceString
+                if (price === null){
+                        price = ""
                     }
-                else if (moneyPicker == "JPY"){ 
-                    priceString = "¥" + priceString
-                }
-                else if (moneyPicker == "AUD"){ 
-                    priceString = "$" + priceString
-                }
-
+                    
+                    else{price = "$" + price}
                 var dateTime = data.events[i].datetime_local
                 var finalTime = dateTime.replace("T", "   ")
+                console.log(data.events[i].stats.average_price)
                 var article = `
                     <article>
                         <p class = "title">${data.events[i].title}</p>
                         <p class = type>${data.events[i].type}</p>
                         <p class = "dateTime">${finalTime}</p>
                         <p class = "location">${data.events[i].venue.name}</p>
-                        <p class = "price">${priceString}</p>
+                        <p class = "price">${price}</p>
                     </article>
                 `
                 console.log(article)
