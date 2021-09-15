f27b6abff0e235e3ae43a1beb0cdc9fd3c43cbfa
Rebecca Sundquist <rsundquist629@gmail.com>
2021-09-14 20:06:29 -0400

    merging new script

--- assets/script.js
+++ assets/script.js
@@ -13,129 +13,107 @@ var results = document.getElementById("results")
 var currencyExchange = "https://v6.exchangerate-api.com/v6/ab0f110ed559d90d33353768/latest/USD"
 //checks to see if input is 5 numbers
 var zipCodeRegex = /^\d{5}$/;
-// searchBar.addEventListener("mouseover", function() {
-    
-    //         var values = [],
-    //             keys = Object.keys(localStorage),
-    //             i = keys.length;
-    
-    //         while ( i-- ) {
-        //             values.push( localStorage.getItem(keys[i]) );
-        //         }
-        //         console.log(values);
-        // })
-        seachButton.addEventListener("click", function() {
-            //conditional statement to stop if not a zipcode is input
-            if(zipCodeRegex.test(searchBar.value) === false ){
-                results.innerHTML = "<h2>Please enter a valid US zipcode<h2>"
-                return
-            }
-            //stores search query to local storage
-            var n = localStorage.getItem(searchBar.value);
-            if (n === null) {
-                n = 0;
-            }
-            n++;
-            localStorage.setItem(searchBar.value, n);
-            //uses exchange rate api to get the conversion ratio for prices from USD to an of the others
-            var moneyPicker = document.getElementById("moneyPicker").value
-            var converter = 1
-            fetch(currencyExchange)
-            .then(response => response.json())
-            .then(data =>  {
-                //lots of parses here to make sure the conversion ratio is a number
-                if (moneyPicker == "CAD") {
-                    var ratio = data.conversion_rates.CAD
-                    console.log(ratio)
-                    converter = parseFloat(ratio)
-                    console.log(converter)
-        }
-        else if (moneyPicker == "EUR") {
-            var ratio = data.conversion_rates.EUR
-            console.log(ratio)
+seachButton.addEventListener("click", function() {
+    //conditional statement to stop if not a zipcode is input
+    if(zipCodeRegex.test(searchBar.value) === false ){
+        results.innerHTML = "<h2>Please enter a valid US zipcode<h2>"
+        return
+    }
+    //stores search query to local storage
+    var n = localStorage.getItem(searchBar.value);
+    if (n === null) {
+        n = 0;
+    }
+    n++;
+    localStorage.setItem(searchBar.value, n);
+    //uses exchange rate api to get the conversion ratio for prices from USD to any of the others
+    var moneyPicker = document.getElementById("moneyPicker").value
+    var converter = 1
+    console.log("getting conversion rate")
+    fetch(currencyExchange)
+    .then(response => response.json())
+    .then(data =>  { 
+        //lots of parses here to make sure the conversion ratio is a number
+        if (moneyPicker == "CAD") {
+            var ratio = data.conversion_rates.CAD
             converter = parseFloat(ratio)
-            console.log(converter)
-        }
-        else if (moneyPicker == "JPY") {
-            var ratio = data.conversion_rates.JPY
-            console.log(ratio)
-            converter = parseFloat(ratio)
-            console.log(converter)
-        }
-        else if (moneyPicker == "AUD") {
-            var ratio = data.conversion_rates.AUD
-            console.log(ratio)
-            converter = parseFloat(ratio)
-            console.log(converter)}
-            else {
-                converter = 1 
             }
-        })
-//tracks which radio button if any is pressed
+            else if (moneyPicker == "EUR") {
+                var ratio = data.conversion_rates.EUR
+                converter = parseFloat(ratio)
+            }
+            else if (moneyPicker == "JPY") {
+                var ratio = data.conversion_rates.JPY   
+                converter = parseFloat(ratio)
+            }
+            else if (moneyPicker == "AUD") {
+                var ratio = data.conversion_rates.AUD
+                converter = parseFloat(ratio)
+            }
+                else {
+                    converter = 1 
+                }
+        
+    console.log("after conversion rate")
+    //tracks which radio button if any is pressed
     var taxonomies = ""
     if (radioSports.checked == true) {
         taxonomies = '&taxonomies.name=sports'
     }
-    else if (radioMusic.checked == true){
-        taxonomies = "&taxonomies.name=concert"
-    }
-//fetches event data from seatgeeks api
+        else if (radioMusic.checked == true){
+            taxonomies = "&taxonomies.name=concert"
+        }
+    //fetches event data from seatgeeks api
     var requestSports = 'https://api.seatgeek.com/2/events?client_id=MjMxMzE1Njl8MTYzMDM3MTYzMS44ODg0NzI&geoip=' + searchBar.value + '&range=10mi' + taxonomies
     var priceString = 0
+    console.log("before price request")
     fetch(requestSports)
         .then(response => response.json())
-        .then(data => {
+        .then(eventsData => {
             var finalHTML = ""
-           
-           
-            for (var i = 0; i < data.events.length ; i++){
-            var price = data.events[i].stats.average_price
+            for (var i = 0; i < eventsData.events.length ; i++){
+            var price = eventsData.events[i].stats.average_price
             if (price === null){
                     priceString = "No Price Listed"
                 }
             else {
                 var priceNum = parseFloat(price)
                 priceNum = priceNum*converter 
-                console.log(converter)
-                console.log(priceNum)
                 priceString =  priceNum.toFixed(2)
                 //adds appropiate dollar sign to price
                 if (moneyPicker == "USD"){
-                    priceString = "$" + priceString    
+                    priceString = "$" + priceString   
                 }
                 else if (moneyPicker == "CAD"){
                     priceString = "$" + priceString
-                    
-                        
                 }
                 else if (moneyPicker == "EUR"){
                     priceString = "€" + priceString
                     }
                 else if (moneyPicker == "JPY"){ 
                     priceString = "¥" + priceString
-                    console.log(converter)
                 }
                 else if (moneyPicker == "AUD"){ 
                     priceString = "$" + priceString
-                    console.log(converter)
                 }
             }   
-//preps the data from api into an HTML format
-                results.innerHTML = ''
-                var dateTime = data.events[i].datetime_local
-                var finalTime = dateTime.replace("T", "   ")
-                var article = `
-                    <article class = "resultBox">
-                        <p class = "title">${data.events[i].title}</p>
-                        <p class = type>${data.events[i].type}</p>
-                        <p class = "dateTime">${finalTime}</p>
-                        <p class = "location">${data.events[i].venue.name}</p>
-                        <p class = "price">${priceString}</p>
-                    </article>
-                `
-                finalHTML += article
+            //preps the data from api into an HTML format
+            results.innerHTML = ''
+            var dateTime = eventsData.events[i].datetime_local
+            var finalTime = dateTime.replace("T", "   ")
+            var article = `
+                <article class = "resultBox">
+                    <p class = "title">${eventsData.events[i].title}</p>
+                    <p class = type>${eventsData.events[i].type}</p>
+                    <p class = "dateTime">${finalTime}</p>
+                    <p class = "location">${eventsData.events[i].venue.name}</p>
+                    <p class = "price">${priceString}</p>
+                </article>
+            `
+            finalHTML += article
         }
             //pastes that data as search results
             results.innerHTML = finalHTML;
         })
+    })
 });
